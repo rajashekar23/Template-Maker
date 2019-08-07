e94094e88ec8d9f7692b93b6b747af8f844560c6
rajashekar23 <rajashekar.dasapathi@gmail.com>
2019-08-07 19:57:25 +0530

    commit

--- src/app/pages/create-page/create-page.component.ts
+++ src/app/pages/create-page/create-page.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
+
 @Component({
   selector: 'app-create-page',
   templateUrl: './create-page.component.html',
   styleUrls: ['./create-page.component.css']
 })
+
 export class CreatePageComponent implements OnInit {
   public Editor = ClassicEditor;
-
   public model = {
 	  name: 'John',
 	  surname: 'Doe',
-		description: '<p>A <b>really</b> nice fellow.</p>'
+    description: '<p>A <b>really</b> nice fellow.</p>',
 	};
   constructor() { }
 
